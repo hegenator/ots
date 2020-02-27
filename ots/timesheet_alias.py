362f3b55a8cb110c5605abc3b3d854aa65581596
Santeri Valjakka <santeri.valjakka@avoin.systems>
2020-02-28 00:00:33 +0200

    Add first alias implementation. Attempts at connecting with Odoo

--- ots/timesheet_alias.py
+++ ots/timesheet_alias.py
@@ -1,3 +1,4 @@
+import click
 from persistent import Persistent
 from .timesheet import TimeSheet
 
@@ -10,7 +11,6 @@ class TimeSheetAlias(Persistent):
                  task_id=None,
                  description="",
                  task_code="",
-                 task_title="",
                  ):
         """
         Stuff and shit
@@ -29,8 +29,17 @@ class TimeSheetAlias(Persistent):
         self.task_id = task_id
 
         self.task_code = task_code
-        self.task_title = task_title
         self.description = description
+
+        self.task_title = ""
+        self.project_title = ""
+
+        try:
+            self.update()
+        except:
+            click.echo("Something went wrong with updating an Alias from Odoo. "
+                       "As a result, you get this nice error message with no useful information "
+                       "at all")
 
     def __repr__(self):
         string_repr = ""
@@ -43,6 +52,17 @@ class TimeSheetAlias(Persistent):
                 string_repr = self.description
 
         return string_repr
+
+    def update(self):
+        """
+        Update the information from Odoo. Basically, try to get the task_id and project_id
+        based on the task code, along with the task and project names.
+
+        If task code not provided, but task_id or project_id have been given by the user,
+        update the names based on those.
+        :return:
+        """
+        pass
 
     def generate_timesheet(self):
         return TimeSheet(
